257e81d85fa8cbd972f1d6728667f99e3c9163b1
gracemepa <gracemepastephoney@students.polmed.ac.id>
2024-10-31 12:16:38 +0700

    baru lagi

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('login');
+// });
+
+// Route::get('/register', function () {
+//     return view('register');
+// });
+
 Route::get('/', function () {
-    return view('page');
+    return view('dashboard.index');
 });
